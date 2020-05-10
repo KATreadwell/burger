8ac4b350b3f2bd8895a5063d064901f853f8334c
KAT <katreadwell1@gmail.com>
2020-05-09 23:01:12 -0700

    setup ORM

--- config/orm.js
+++ config/orm.js
@@ -6,11 +6,33 @@ let connection = require("../config/connection")
 // These are the methods you will need to use in order to retrieve and store data in your database.
 
 // selectAll()
-
-function selectAll()
-
-
 // insertOne()
 // updateOne()
 
 // Export the ORM object in module.exports.
+
+const orm = {
+    connection: connection,
+    selectAll: (cols, table) => {
+        connection.query("SELECT ?? FROM ??", [cols, table], (err, res) => {
+            if (err) { throw err; }
+            console.log(res);
+        })
+    },
+    insertOne: (cols, table, vals, cb) => {
+        connection.query("INSERT INTO " + table, cols, vals, (err, res) => {
+            if (err) { throw err; }
+            console.log(res);
+        })
+    },
+    insertOne: (cols, table, vals, cb) => {
+        connection.query("INSERT INTO " + table, cols, vals, (err, res) => {
+            if (err) { throw err; }
+            console.log(res);
+        })
+    },
+
+
+}
+
+module.exports = orm;
